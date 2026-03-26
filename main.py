05654b2a252baf5f23d75d4dd353fc60e6514030
Luan Mercaldi <luanwpm@icloud.com>
2026-03-26 20:33:30 -0300

    Adicionado o menu de interação que pergunta sobre as culturas

--- main.py
+++ main.py
@@ -15,8 +15,20 @@ while True:
     print('2. Listar Dados')
     print('0. Sair')
 
+    #Pede pro usuário escolher a cultura
     opcao = input('Escolha uma opção: ')
     if opcao == '1':
+        print('\n--- Cadastro de Novo Plantio ---')
+        print('Qual a cultura? (1. Café | 2. Soja')
+        escolha = input('Escolha o número: ')
+        if escolha == '1':
+            cultura = 'Café'
+        elif escolha == '2':
+            cultura = 'Soja'
+        else:
+            print('Opção Invalida! Usando INDEFINIDO.')
+            cultura = 'INDEFINIDO'
+
         # Pede pro usuário inserir alguns dados
         base = float(input('Digite a base: '))
         altura = float(input('Digite a altura: '))
@@ -24,9 +36,10 @@ while True:
         # Resultado do calculo da area e adição na lista
         area_triangulo = calcular_area_triangulo(base, altura)
         lista_areas.append(area_triangulo)
+        lista_culturas.append(cultura)
 
         # Mostra o resultado
-        print(f'A area mede {area_triangulo:.2f} metros²')
+        print(f'Cultura {cultura} cadastrada com area {area_triangulo:.2f} m².')
 
         pass
     elif opcao == '0':
